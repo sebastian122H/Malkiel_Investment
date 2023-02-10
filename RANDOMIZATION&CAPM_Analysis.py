e13d9725a02f07cb8c20813559abf53e5da8dc5a
Arthur Birnstiel abirnstiel@icloud.com <abirnstiel@icloud.com>
2023-02-10 13:54:50 +0100

    reworked entire code, test functionality

--- RANDOMIZATION&CAPM_Analysis.py
+++ RANDOMIZATION&CAPM_Analysis.py
@@ -3,63 +3,53 @@ import pandas as pd
 import numpy as np
 ## Parsing CSV
 
-etoro_data = pd.read_csv("etoro_data.csv")
-stock_list = etoro_data["Ticker"].values.tolist()
+etoro_raw = pd.read_csv("etoro_data.csv")
+stock_list = etoro_raw["Ticker"].values.tolist()
 
 stock_num = int(input("How many stocks does every group need?: "))
 # lets loop this instead
-j = 1
+k = 1
 all_picks = []
-for j in range(1,5):
+for k in range(1,5):
     group_picks = np.random.choice(stock_list,stock_num,replace=False)
-    print("Group ", j, "stocks are: ", group_picks)
+    print("Group ", k, "stocks are: ", group_picks)
     all_picks.extend(group_picks)
-    j+=1
-
 
 ## Define Market Rate and Risk free Rate
 # we should try to make this dynamic too ideally
 mrkt_ret = 0.008
 risk_f = 0.0346
 
-## Market Returns and Download
-dfm = yf.download("SPY", period="5y", interval="1wk", ignore_tz = True, prepost = False)["Close"]
-
-market_returns = []
-for i in range(len(dfm)):
-    if i +1 < len(dfm):
-        market_returns.append((dfm.iloc[i + 1] - dfm.iloc[i]) / dfm.iloc[i])
-    else:
-        market_geo_returns = [i + 1 for i in market_returns]
-        df_mreturns = pd.DataFrame({"Ret" : market_returns, "Ret + 1" : market_geo_returns})
-        variance_market = np.var(df_mreturns["Ret"])
+# Create an array with the weekly market returns
+market_data = yf.download("SPY", period="5y", interval="1wk", group_by='ticker')
+market_returns = market_data['Adj Close'].pct_change().to_list()
+market_returns.pop(0)
+# calculate variance based on this
+variance_market = np.var(market_returns)
+# what we need geo return for??
+# market_geo_returns = [i + 1 for i in market_returns]
+# df_mreturns = pd.DataFrame({"Ret" : market_returns, "Ret + 1" : market_geo_returns})
 
 ##Stock returns and Download
-
-dfh = yf.download(all_picks, period="5y", interval="1wk", ignore_tz = True, prepost = False)["Close"]
+market_data = yf.download("SPY", period="5y", interval="1wk", group_by='ticker')
+market_returns = market_data['Adj Close'].pct_change().to_list()
+stock_data = yf.download(all_picks, period="5y", interval="1wk", ignore_tz = True, prepost = False)["Adj Close"]
 
 
 for j in range(len(all_picks)):
-    returns = []
-    for i in range(len(dfh)):
-        if i +1 < len(dfh):
-            returns.append((dfh.iloc[i + 1,j] - dfh.iloc[i,j]) / dfh.iloc[i,j])
-        else:
-            continue
-    geo_returns = [i + 1 for i in returns]
-    df_returns = pd.DataFrame({"Ret" : returns, "Ret + 1" : geo_returns})
-    Weeks = len(df_returns)
-    Years = len(df_returns)/52
-    aar_arith = np.average(df_returns["Ret"])*52
-    aar_geo = (np.product(df_returns["Ret + 1"])**(1/Years)) - 1
-    variance_stock = np.var(df_returns["Ret"])
-    std_deviation = np.std(df_returns["Ret"])
-    if std_deviation != 0:
-        sharpe_arith = (aar_arith/std_deviation)
-        sharpe_geo = (aar_geo/std_deviation)
-        covariance = np.cov(df_returns["Ret"],df_mreturns["Ret"])
-        beta = covariance[0,1]/variance_market
-        CAPM = risk_f + beta * (mrkt_ret - risk_f)
-        print("Stock: ",stock_list[j], "\n", "CAPM: " ,CAPM, "\n", "Beta: ", beta, "\n", "Sharpe geo/arith: (", sharpe_arith, "/", sharpe_geo, ")" "\n", "AAR geo/arith: (",
-        aar_geo, "/", aar_arith,")", "\n" "Std. Dev: ",std_deviation, "\n")
+    stock_returns = stock_data[all_picks[j]].pct_change().to_list()
+    # check that this does not count nan!!!!!!!!!!!
+    weeks = len(stock_returns)
+    years = weeks/52
+    mean_return = stock_returns.mean()
+    std_return = stock_returns.std()
+
+    aar_arith = mean_return * 52
+    variance_stock = stock_returns.var()
+    sharpe_arith = (aar_arith/std_return)
+    covariance = np.cov(stock_returns,market_returns)
+    beta = covariance/variance_market
+    # warum nicht market return?
+    CAPM = risk_f + beta * (mrkt_ret - risk_f)
+    print("Stock: ",all_picks[j], "\n", "CAPM: " ,CAPM, "\n", "Beta: ", beta, "\n", "Sharpe arith: (", sharpe_arith, ")" "\n", "AAR arith: (", aar_arith,")", "\n" "Std. Dev: ",std_return, "\n")
 
